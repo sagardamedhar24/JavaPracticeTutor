a16350122aae83d7fb409788e95a356b2c5b7ef9
sagar <sagar@192.168.0.6>
2022-04-20 01:37:15 +0530

    Commit Third

---
+++ JavaPracticeTutors/src/IterationDemo2.java
@@ -0,0 +1,25 @@
+/*
+ This is a another pattern demo
+ 
+ 
+ */
+
+
+
+
+public class IterationDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=5;i>=1;i--) {
+			
+		for(int j=0;j<=i;j++) {
+			System.out.print("* ");
+		}
+		System.out.println(" ");
+	}
+		
+
+	}
+
+}
