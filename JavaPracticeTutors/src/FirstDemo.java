a8e692496b7127fb3879029dd6aeec33383edde1
sagar <sagar@192.168.0.6>
2022-04-20 01:20:55 +0530

    First Commit

---
+++ JavaPracticeTutors/src/FirstDemo.java
@@ -0,0 +1,10 @@
+
+public class FirstDemo {
+	public static void main(String[] args) {
+		int num =5;
+		double dob = Double.valueOf(num);
+		System.out.println(num);
+		System.out.println(dob);
+	}
+
+}
